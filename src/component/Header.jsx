9a4e7b8da2396c124de13544abd31b539ccf504e
Ismaila Saidu <ismailasaidu997@gmail.com>
2023-03-01 15:51:50 -0500

    adding existing project files to git

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import account from "../Assets/account.png";
-import cart from "../Assets/cart.png";
+import Cart from "../Assets/cart.png";
 import hamburger from "../Assets/hamburger.png";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const cart = useSelector((item) => {
+    return item.cart.value;
+  });
   const navigate = useNavigate();
   return (
     <div className="flex items-center justify-between pt-10 pr-14 pl-14 h-32">
@@ -34,10 +38,10 @@ const Header = () => {
       <div className="flex items-center gap-5">
         <img src={account} alt="nav-icons" />
         <div className="relative " >
-          <img src={cart} alt="nav-icons" onClick={()=>{
+          <img src={Cart} alt="nav-icons" onClick={()=>{
             navigate("/cart")
           }}/>
-          <div className="absolute bottom-3 left-3 bg-blue rounded-full w-4 h-4" ><p className="ml-1 text-xs  text-white">2</p></div>
+          <div className="absolute bottom-3 left-3 bg-blue rounded-full w-4 h-4" ><p className="ml-1 text-xs  text-white">{cart.length}</p></div>
         </div>
 
         <img src={hamburger} alt="nav-icons" />
